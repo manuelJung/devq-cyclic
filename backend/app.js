25ec098660ea83bf53fda91a723da9a123be1d39
manuelJung <manuel.jung.wwi12@gmail.com>
2022-08-24 16:02:26 +0200

    setup eslint

--- backend/app.js
+++ backend/app.js
@@ -14,6 +14,7 @@ app.use((req, res, next) => {
   next(error)
 })
 
+// eslint-disable-next-line no-unused-vars
 app.use((error, req, res, next) => {
   res.status(error.status || 500).send({
     error: error.message
