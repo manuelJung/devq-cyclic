6a445f0dfc73cd4f0709de8b0b06d5645f2e4911
manuelJung <manuel.jung.wwi12@gmail.com>
2022-08-25 15:29:42 +0200

    created frontend

--- frontend/src/routes/create-question/index.js
+++ frontend/src/routes/create-question/index.js
@@ -1,10 +1,27 @@
+import * as React from 'react'
 import './index.scss'
 import Layout from '../../Layout'
 
 export default function CreateQuestion () {
+  const [title, setTitle] = React.useState('')
+  const [description, setDescription] = React.useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    alert('submit')
+  }
+
   return (
     <Layout>
-      <h1>Create-Question Route</h1>
+      <form className='CreateQuestion' onSubmit={handleSubmit}>
+        <h2>Title</h2>
+        <input placeholder='Title' value={title} onChange={e => setTitle(e.target.value)}/>
+
+        <h2>Beschreibung</h2>
+        <textarea rows={20} value={description} onChange={e => setDescription(e.target.value)}/>
+
+        <button>Absenden</button>
+      </form>
     </Layout>
   )
 }
